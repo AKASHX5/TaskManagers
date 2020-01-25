60908fa5bb2a0149d0491f4f2102174d062626e4
AKASHX5 <akashnsu2016@gmail.com>
2020-01-25 22:49:20 +0600

    login and signup with jwt added

--- taskuser/tests.py
+++ taskuser/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+import base64
+import json
 
 # Create your tests here.
+
+
+def create_user(username = 'test@test.com',password = 'cft6'):
+    return get_user_model().objects.create_user(
+        username=username,
+        password = password
+    )
+
+
+class AuthenticationTest(APITestCase):
+    def test_user_can_sign_up(self):
+        response = self.client.post(reverse('sign_up'), data={
+            'username':'test@test.com',
+            'password1': "cft6",
+            'password2': 'cft6',
+
+        })
+        user = get_user_model().objects.last()
+        self.assertEqual(status.HTTP_201_CREATED,response.status_code)
+        self.assertEqual(response.data['id'],user.id)
+        self.assertEqual(response.data['username'], user.username)
+
+        def test_user_can_log_in(self):
+            user = create_user()
+            response = self.client.post(reverse('log_in'), data={
+                'username': user.username,
+                'password': 'cft6',
+            })
+
+            access = response.data['access']
+            header, payload, signature = access.split('.')
+            decoded_payload = base64.b64decode(f'{payload}==')
+            payload_data = json.loads(decoded_payload)
+
+            self.assertEqual(status.HTTP_200_OK, response.status_code)
+            self.assertIsNotNone(response.data['refresh'])
+            self.assertEqual(payload_data['id'], user.id)
+            self.assertEqual(payload_data['username'], user.username)
+
--- taskuser/views.py
+++ taskuser/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer, LogInSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import viewsets
+from rest_framework import permissions,request
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class SignUpView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class LogInView(TokenObtainPairView):
+    serializer_class = LogInSerializer
